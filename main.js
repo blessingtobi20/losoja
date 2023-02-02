1ccb15977ab57862b99a12d200902e497c945240
tobi <asebiekoblessing@gmail.com>
2023-02-02 13:56:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ document.querySelector('.loader').style.display = 'flex';
 // Hide the loader after a few seconds (for demonstration purposes)
 setTimeout(function() {
   document.querySelector('.loader').style.display = 'none';
-}, 3);
+}, );
 
 
 window.onload = function() {
