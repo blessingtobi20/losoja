335ebae976f95faf61e262905aabc6d4e2ccfd44
tobi <asebiekoblessing@gmail.com>
2023-02-02 13:17:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,3 +5,5 @@ document.querySelector('.loader').style.display = 'flex';
 setTimeout(function() {
   document.querySelector('.loader').style.display = 'none';
 }, 20);
+
+window.addEventListener('load', fadeEffect);
