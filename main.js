0483ae70cc2d484e3989d4f2f78074cc458e03bb
tobi <asebiekoblessing@gmail.com>
2023-02-02 13:45:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,3 +6,7 @@ setTimeout(function() {
   document.querySelector('.loader').style.display = 'none';
 }, 3);
 
+window.addEventListener('load', function() {
+    document.querySelector('.loader').style.opacity = '0'
+  });
+  
